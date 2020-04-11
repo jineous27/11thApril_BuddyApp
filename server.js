078c5532e4b51d4ba4d097ae2083384637816448
jinheeyi <jinhee.yi27@gmail.com>
2020-04-12 07:12:33 +1000

    Set up 서버

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-const expres
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 2222;
+app.listen(port, console.log(`Server is running on port ${port}`));
+
