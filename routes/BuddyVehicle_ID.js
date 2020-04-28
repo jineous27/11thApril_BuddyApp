d4fce3db8b8548afcad72e18e45c388481e9c214
jinheeyi <jinhee.yi27@gmail.com>
2020-04-29 08:04:44 +1000

    DB 설계하기 - Buddy Vehicle & Order Status

--- routes/BuddyVehicle_ID.js
+++ routes/BuddyVehicle_ID.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const buddyVehicleIdModel = require("../models/BuddyVehicle_ID");
 
 //CRUD Create Retrieve Update Delete
 
@@ -15,10 +16,35 @@ router.get ('/', (req, res) => {
 
 // Post Buddy's Vehicle ID
 router.post ('/', (req, res) => {
-    res.json ({
-        msg: "Successfully POST Buddy's Vehicle ID"
+    const newbuddyVehicleId = new buddyVehicleIdModel ({
+        buddy_vehicle_id: req.body.buddy_vehicle_id,
+        delivery_buddy_id: req.body.delivery_buddy_id,
+        vehicle_registration_number: req.body.vehicle_registration_number,
+        vehicle_model: req.body.vehicle_model,
+        vehicle_color: req.body.vehicle_color,
+        vehicle_insurance: req.body.vehicle_insurance,
+        vehicle_insurance_documents: req.body.vehicle_insurance_documents
+    })
+    
+    newbuddyVehicleId
+    .save()
+    .then(result => {
+        res.status(201).json({
+            message: "Handling POST requests to buddy ID",
+            createdBuddyIdInfo: result
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err.message
+        });
     });
+
+    // res.json ({
+    //     msg: "Successfully POST Buddy's Vehicle ID"
+    // });
 });
+
 
 
 // Patch Buddy's Vehicle ID
--- routes/DeliveryOrderStatus.js
+++ routes/DeliveryOrderStatus.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const deliveryOrderStatusModel = require("../models/DeliveryORderStatus")
 
 //CRUD Create Retrieve Update Delete
 
@@ -14,9 +15,35 @@ router.get ('/', (req, res) =>{
 // Get Certain Delivery Order Status ID
 // Post Delivery Order Status ID
 router.post ('/', (req, res) => {
-    res.json ({
-        msg: "Successfully POST Delivey Order Status ID"
+    const newdeliveryOrderStatusId = new deliveryOrderStatusModel({
+        delivery_status_id: req.body.delivery_status_id,
+        buddy_task_id: req.body.buddy_task_id,
+        delivery_order_status: req.body.delivery_order_status,
+        time_to_accept: req.body.time_to_accept,
+        time_to_pickup: req.body.time_to_pickup,
+        time_to_complete: req.body.time_to_complete,
+        time_to_incomplete: req.body.time_to_incomplete,
+        time_to_cancel: req.body.time_to_cancel
     });
+
+    newdeliveryOrderStatusId
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: "Handling POST requests to buddy id",
+                createdBuddyIdInfo: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json ({
+                error: err.message
+            });
+        });
+
+
+    // res.json ({
+    //     msg: "Successfully POST Delivey Order Status ID"
+    // });
 });
 
 
